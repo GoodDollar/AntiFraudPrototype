0be3b570657dabc5215547e2d5b751cba7f6356b
Josh McMillan <josh@joshmcmillan.co.uk>
2019-02-27 15:04:39 +0000

    Add Administrate

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   resources :users, only: [:create] do
     collection do
       post 'login'
